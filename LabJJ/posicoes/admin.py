2559b7d64dd76400bfdce28484cfb81dedaa34eb
Arthur Reigada Prioli Menezes <arthur.prioli@gmail.com>
2025-11-24 09:48:28 -0300

    Separa o back-end do front-end

--- LabJJ/posicoes/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from posicoes.models import BJJPos
-
-admin.site.register(BJJPos)
--- LabJJ/posicoes/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from posicoes.models import BJJPos
-
-
-class BJJPosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BJJPos
-        fields = "__all__"
